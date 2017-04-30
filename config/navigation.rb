8bdbeae3bfbb335fc8d9924233e07d969a2e98c9
swatantra7 <swatantrajha7@gmail.com>
2017-04-30 14:08:59 +0530

    this commit refs - initial commit with high voltage

--- config/navigation.rb
+++ config/navigation.rb
@@ -2,10 +2,10 @@
 # Configures your navigation
 SimpleNavigation::Configuration.run do |navigation|
   navigation.items do |primary|
-    primary.item :ABOUTUS, 'ABOUT US', '#' do |items|
-    	items.item :about, 'ABOUT US', '#'
-    end
-    primary.item :HOME, 'HOME', '#'
+    primary.item :HOME, :HOME, page_path('home')
+    primary.item :MOBILE, 'MOBILE', '#'
+    primary.item :COMPARE, 'COMPARE', '#'
+    primary.item :HOME, :ABOUT, page_path('about')
    	primary.dom_class = 'nav navbar-nav'
     primary.dropdown = true
     primary.split = false
